34e56ddef517e426ae3126e744905f0f6148198b
lucswart <66739939+lucswart@users.noreply.github.com>
2020-10-31 19:20:50 +0100

    disabled overflow

--- chat-app/src/Chat.js
+++ chat-app/src/Chat.js
@@ -51,18 +51,20 @@ function Chat({ messages }) {
         </div>
       </div>
 
-      <ScrollToBottom className="chat__body">
-        {messages.map((message) => (
-          <p
-            className={`chat__message ${
-              message.name == user?.displayName && "chat__receiver"
-            }`}
-          >
-            <span className="chat__name">{message.name}</span>
-            <span className="chat__messagetext">{message.message}</span>
-            <span className="chat__timestamp">{message.timestamp}</span>
-          </p>
-        ))}
+      <ScrollToBottom>
+        <div className="chat__body">
+          {messages.map((message) => (
+            <p
+              className={`chat__message ${
+                message.name == user?.displayName && "chat__receiver"
+              }`}
+            >
+              <span className="chat__name">{message.name}</span>
+              <span className="chat__messagetext">{message.message}</span>
+              <span className="chat__timestamp">{message.timestamp}</span>
+            </p>
+          ))}
+        </div>
       </ScrollToBottom>
       <div className="chat__footer">
         <InsertEmoticonIcon />
